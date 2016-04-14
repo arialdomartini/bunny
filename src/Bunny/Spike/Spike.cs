42d490a4f0f651a3075a6d528969e0120179398a
Arialdo Martini <arialdomartini@gmail.com>
2016-04-14 23:14:29 +0200

    Consumer's crash is detected by the Decorator

--- src/Bunny/Spike/Spike.cs
+++ src/Bunny/Spike/Spike.cs
@@ -6,13 +6,36 @@ namespace Spike
   public interface IConsumer
   {
     void ConsumeMessage(string message);
+    void OnCancel();
   }
 
   public class Consumer : IConsumer
   {
     public void ConsumeMessage(string message)
     {
-      Console.WriteLine("Consumer: consuming message");
+      if (message == "make the consumer crash")
+      {
+        throw new Exception("crashed");
+      }
+      Console.WriteLine("  Consumer: consuming message");
+    }
+
+    public void OnCancel()
+    {
+      Console.WriteLine("Consumer: OnCancel");
+    }
+  }
+
+  public interface ISetup
+  {
+    void Setup(Channel channel, IConsumer consumer);
+  }
+
+  public class SetupConsumer : ISetup
+  {
+    public void Setup(Channel channel, IConsumer consumer)
+    {
+      Console.WriteLine("  SetupConsumer: creating queues and registering consumer");
     }
   }
 
@@ -27,8 +50,21 @@ namespace Spike
 
     public void ConsumeMessage(string message)
     {
-      Console.WriteLine("Decorator: consuming message");
-      _consumer.ConsumeMessage(message);
+      Console.WriteLine("  Decorator: consuming message");
+      try
+      {
+        _consumer.ConsumeMessage(message);
+      }
+      catch (Exception)
+      {
+        Console.WriteLine("  Decorator: consumer crashed");
+        OnCancel();
+      }
+    }
+
+    public void OnCancel()
+    {
+      Console.WriteLine("  Consumer: OnCancel");
     }
   }
 
@@ -40,7 +76,11 @@ namespace Spike
       var consumer = new Consumer();
       var decorator = new Decorator(consumer);
 
+      var setupConsumer = new SetupConsumer();
+      setupConsumer.Setup(new Channel(), decorator);
+
       decorator.ConsumeMessage("foo");
+      decorator.ConsumeMessage("make the consumer crash");
     }
   }
 }
