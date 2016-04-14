bef57278ee43e0fc848c6acc7c30e11eee2afd60
Arialdo Martini <arialdomartini@gmail.com>
2016-04-14 22:59:24 +0200

    Decorator, by using inheritance

---
+++ src/Bunny/Spike/Spike.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace Spike
+{
+  public interface IConsumer
+  {
+    void ConsumeMessage(string message);
+  }
+
+  public class Consumer : IConsumer
+  {
+    public void ConsumeMessage(string message)
+    {
+      Console.WriteLine("Consumer: consuming message");
+    }
+  }
+
+  public class Decorator : IConsumer
+  {
+    private readonly IConsumer _consumer;
+
+    public Decorator(IConsumer consumer)
+    {
+      _consumer = consumer;
+    }
+
+    public void ConsumeMessage(string message)
+    {
+      Console.WriteLine("Decorator: consuming message");
+      _consumer.ConsumeMessage(message);
+    }
+  }
+
+  internal class Test
+  {
+    [Test]
+    public void use_case()
+    {
+      var consumer = new Consumer();
+      var decorator = new Decorator(consumer);
+
+      decorator.ConsumeMessage("foo");
+    }
+  }
+}
